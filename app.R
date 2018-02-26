811629c12f285e77378cfbd7b6e4cd7d8d57d9bf
Matthew Phelps <mPhelps@hjerteforeningen.dk>
2018-02-26 16:31:53 +0100

    new radio buttons
    with JS styling. Uses shinyWidgets

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(riskvisrr)
 library(data.table)
 library(magrittr)
+library(shinyWidgets)
 stroke.dt <- data.table(stroke1yr)
 
 # Re-order columns to the order patients will enter their variables information
@@ -26,7 +27,6 @@ new.var.order <-  c("age",
                     "year2000")
 
 setcolorder(stroke.dt,new.var.order)
-
 setkeyv(stroke.dt, new.var.order)
 
 
@@ -39,48 +39,73 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
+      tags$style("#user_age {font-size:38px;height:50px; width: 110px;}"),
       numericInput(inputId = "user_age",
-                   "What is your age?", value = NULL,
+                   "Patient's age:", value = NULL,
                    min = 20, max = 99),
-      radioButtons(inputId = "sex",
-                   "Are you male or female?",
+
+      radioGroupButtons(inputId = "sex",
+                   label = "Male or female:",
                    selected = character(0),
-                   choices = c("Male" = "no",
-                               "Female" = "yes")),
-      radioButtons(inputId = "stroke",
+                   choiceNames = c("Male", "Female"),
+                   choiceValues = c("no", "yes")),
+      radioGroupButtons(inputId = "stroke",
                    "Have you ever had a stroke?",
                    selected = character(0),
-                   choices = c("No" = "no",
-                               "Yes" = "yes")),
-      radioButtons(inputId = "hf",
+                   choiceNames = c("No ", "Yes"),
+                   choiceValues = c("no", "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   justified = TRUE, width = "140px"),
+      radioGroupButtons(inputId = "hf",
                    "Have you previously had heart failure?",
                    selected = character(0),
-                   choices = c("No" = "no",
-                               "Yes" = "yes")),
-      radioButtons(inputId = "diabetes",
+                   choiceNames = c("No ", "Yes"),
+                   choiceValues = c("no", "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   justified = TRUE, width = "140px"),
+      radioGroupButtons(inputId = "diabetes",
                    "Have you ever had a diabetes?",
                    selected = character(0),
-                   choices = c("No" = "no",
-                               "Yes" = "yes")),
-      radioButtons(inputId = "hyperT",
+                   choiceNames = c("No ", "Yes"),
+                   choiceValues = c("no", "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   justified = TRUE, width = "140px"),
+      radioGroupButtons(inputId = "hyperT",
                    "Have you ever had a hypertension?",
                    selected = character(0),
-                   choices = c("No" = "no",
-                               "Yes" = "yes")),
-      radioButtons(inputId = "vasc",
+                   choiceNames = c("No ", "Yes"),
+                   choiceValues = c("no", "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   justified = TRUE, width = "140px"),
+      radioGroupButtons(inputId = "vasc",
                    "Do you have vascular disease?",
                    selected = character(0),
-                   choices = c("No" = "no",
-                               "Yes" = "yes")),
-      numericInput(inputId = "chadsvasc",
-                   "What is your CHADSVASC score?", value = 0,
-                   min = 0, max = 7)
+                   choiceNames = c("No ", "Yes"),
+                   choiceValues = c("no", "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   justified = TRUE, width = "140px"),
+      # Color buttons that are higher risk. From:
+      # https://github.com/dreamRs/shinyWidgets/issues/41
+      tags$script("$(\"input:radio[name='stroke'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
+      tags$script("$(\"input:radio[name='stroke'][value='no']\").parent().css('background-color', '#B2EDB5');"),
+      
+      tags$script("$(\"input:radio[name='hf'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
+      tags$script("$(\"input:radio[name='hf'][value='no']\").parent().css('background-color', '#B2EDB5');"),
+      
+      tags$script("$(\"input:radio[name='diabetes'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
+      tags$script("$(\"input:radio[name='diabetes'][value='no']\").parent().css('background-color', '#B2EDB5');"),
+      
+      tags$script("$(\"input:radio[name='hyperT'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
+      tags$script("$(\"input:radio[name='hyperT'][value='no']\").parent().css('background-color', '#B2EDB5');"),
+      
+      tags$script("$(\"input:radio[name='vasc'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
+      tags$script("$(\"input:radio[name='vasc'][value='no']\").parent().css('background-color', '#B2EDB5');")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("distPlot"),
-      tags$h2("Your risk of having a stroke in the next year is between :"),
+      tags$h2("Patient's stroke risk is estimated to be:"),
       tags$h2(strong(textOutput("textRisk"))),
       br(),
       hr(),
@@ -101,32 +126,40 @@ server <- function(input, output) {
       input$user_age >= 20 &&
       input$user_age <= 99
   })
+  
+  is.only.one.value <- reactive({
+    # This is only run if the created expression is evaluated inside another
+    # reactive function - I think
+    !is.na(input$user_age) &&
+      input$user_age >= 20 &&
+      input$user_age <= 99
+  })
   output$textRisk <- renderText({
     
     # Order of subset arguments must be same order as new.col.order variable set in
     # intro.
      # browser()
     if (is.valid.age()) {
-      paste0(stroke.dt[.(input$user_age,
+      
+      dat.sub <- stroke.dt[.(input$user_age,
                             input$sex,
                             input$stroke,
                             input$hf,
                             input$diabetes,
                             input$hyperT,
                             input$vasc),
-                          min(stroke1y)],
+                          range(stroke1y)]
+      cond <- dat.sub[2]-dat.sub[1]<0.001
+      if (cond){
+        paste0(dat.sub[1], "%")
+      } else if (!cond) {
+        paste0("between ", dat.sub[1],
+               "%", " and ",
+               dat.sub[2],
+               "%")
+  }
 
-           "%", " and ",
-
-           stroke.dt[.(input$user_age,
-                            input$sex,
-                            input$stroke,
-                            input$hf,
-                            input$diabetes,
-                            input$hyperT,
-                            input$vasc),
-                          max(stroke1y)],
-           "%")
+           
     }
     else{
       paste0("Please enter your age (between 20 and 99)")
