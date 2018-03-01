d20b5c88c8ec2cca9107b7ff9ecf4e2b81594160
Matthew Phelps <mPhelps@hjerteforeningen.dk>
2018-03-01 16:38:12 +0100

    No working color buttonq
    I cannot get the JQuerry selection on the value. I think the double quotes caused by the deparse(substitute()) method are causing problems

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(riskvisrr)
 library(data.table)
-library(magrittr)
 library(shinyWidgets)
 stroke.dt <- data.table(stroke1yr)
 
@@ -29,6 +28,43 @@ new.var.order <-  c("age",
 setcolorder(stroke.dt,new.var.order)
 setkeyv(stroke.dt, new.var.order)
 
+# functions ---------------------------------------------------------------
+jsButtonColor <- function(var, color, value){
+  # Makes simple js code to color buttons in shinyWidgets library. From:
+  # https://github.com/dreamRs/shinyWidgets/issues/41
+  # var, color and yes/no need to be in quotes
+  paste0("$(\"input:radio[name='",
+         var,
+         "'][value='",
+         paste0("&quote",value, "&quote"),
+         "']\").parent().css('background-color', '",
+         color,
+         "');")  
+}
+depSub <- function(a){
+  # shortcut helper n
+  deparse(substitute(a))
+}
+f <- function(x) substitute(x)
+g <- function(x) {
+  # browser()
+  deparse(f(x))
+}
+class(substitute(1:10))
+class(quote(1:10))
+
+evPar <- function(x){
+  # short helper fun
+  eval(parse(text = x))
+}
+# jsButtonColor("stroke", "EDB6B2", depSub(Yes))
+
+# Variables ---------------------------------------------------------------
+
+button.width <- "210px"
+
+
+# UI ----------------------------------------------------------------------
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -43,63 +79,77 @@ ui <- fluidPage(
       numericInput(inputId = "user_age",
                    "Patient's age:", value = NULL,
                    min = 20, max = 99),
-
+      
       radioGroupButtons(inputId = "sex",
-                   label = "Male or female:",
-                   selected = character(0),
-                   choiceNames = c("Male", "Female"),
-                   choiceValues = c("no", "yes")),
+                        label = "Male or female:",
+                        choiceNames = c(HTML("Not <br/>selected"),
+                                         HTML("Male <br/> "), HTML("Female<br/> ")),
+                        selected =  depSub(c("no", "yes")),
+                        choiceValues = c(depSub(c("no", "yes")),
+                                         depSub("no"),
+                                         depSub("yes"))),
+      
       radioGroupButtons(inputId = "stroke",
-                   "Have you ever had a stroke?",
-                   selected = character(0),
-                   choiceNames = c("No ", "Yes"),
-                   choiceValues = c("no", "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   justified = TRUE, width = "140px"),
+                        "Have you ever had a stroke?",
+                        choiceNames = c( HTML("Not <br/>selected"), "No", "Yes"),
+                        selected =  depSub(c("no", "yes")),
+                        choiceValues = c(depSub(c("no", "yes")),
+                                         depSub("no"),
+                                         depSub("yes")),
+                        checkIcon = list(yes = icon("check")),
+                        justified = TRUE, width = button.width),
       radioGroupButtons(inputId = "hf",
-                   "Have you previously had heart failure?",
-                   selected = character(0),
-                   choiceNames = c("No ", "Yes"),
-                   choiceValues = c("no", "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   justified = TRUE, width = "140px"),
+                        "Have you previously had heart failure?",
+                        choiceNames = c( "Not selected", "No", "Yes"),
+                        selected =  depSub(c("no", "yes")),
+                        choiceValues = c(depSub(c("no", "yes")),
+                                         depSub("no"),
+                                         depSub("yes")),
+                        checkIcon = list(yes = icon("check")),
+                        justified = TRUE, width = button.width),
       radioGroupButtons(inputId = "diabetes",
-                   "Have you ever had a diabetes?",
-                   selected = character(0),
-                   choiceNames = c("No ", "Yes"),
-                   choiceValues = c("no", "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   justified = TRUE, width = "140px"),
+                        "Have you ever had a diabetes?",
+                        choiceNames = c( "Not selected", "No", "Yes"),
+                        selected =  depSub(c("no", "yes")),
+                        choiceValues = c(depSub(c("no", "yes")),
+                                         depSub("no"),
+                                         depSub("yes")),
+                        checkIcon = list(yes = icon("check")),
+                        justified = TRUE, width = button.width),
       radioGroupButtons(inputId = "hyperT",
-                   "Have you ever had a hypertension?",
-                   selected = character(0),
-                   choiceNames = c("No ", "Yes"),
-                   choiceValues = c("no", "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   justified = TRUE, width = "140px"),
+                        "Have you ever had a hypertension?",
+                        choiceNames = c( "Not selected", "No", "Yes"),
+                        selected =  depSub(c("no", "yes")),
+                        choiceValues = c(depSub(c("no", "yes")),
+                                         depSub("no"),
+                                         depSub("yes")),
+                        checkIcon = list(yes = icon("check")),
+                        justified = TRUE, width = button.width),
       radioGroupButtons(inputId = "vasc",
-                   "Do you have vascular disease?",
-                   selected = character(0),
-                   choiceNames = c("No ", "Yes"),
-                   choiceValues = c("no", "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   justified = TRUE, width = "140px"),
-      # Color buttons that are higher risk. From:
-      # https://github.com/dreamRs/shinyWidgets/issues/41
-      tags$script("$(\"input:radio[name='stroke'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
-      tags$script("$(\"input:radio[name='stroke'][value='no']\").parent().css('background-color', '#B2EDB5');"),
+                        "Do you have vascular disease?",
+                        choiceNames = c( "Not selected", "No", "Yes"),
+                        selected =  depSub(c("no", "yes")),
+                        choiceValues = c(depSub(c("no", "yes")),
+                                         depSub("no"),
+                                         depSub("yes")),
+                        checkIcon = list(yes = icon("check")),
+                        justified = TRUE, width = button.width),
       
-      tags$script("$(\"input:radio[name='hf'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
-      tags$script("$(\"input:radio[name='hf'][value='no']\").parent().css('background-color', '#B2EDB5');"),
+      # Color buttons that are higher risk.
+      tags$script("$(\"input:radio[name='stroke'][value='&quot;yes&quot;']\").parent().css('background-color','#EDB6B2');"),
+      tags$script(jsButtonColor("stroke", "#B2EDB5", depSub("no"))),
       
-      tags$script("$(\"input:radio[name='diabetes'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
-      tags$script("$(\"input:radio[name='diabetes'][value='no']\").parent().css('background-color', '#B2EDB5');"),
+      tags$script(jsButtonColor("hf", "#EDB6B2", "yes")),
+      tags$script(jsButtonColor("hf", "#B2EDB5", "no")),
       
-      tags$script("$(\"input:radio[name='hyperT'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
-      tags$script("$(\"input:radio[name='hyperT'][value='no']\").parent().css('background-color', '#B2EDB5');"),
+      tags$script(jsButtonColor("diabetes", "#EDB6B2", "yes")),
+      tags$script(jsButtonColor("diabetes", "#B2EDB5", "no")),
       
-      tags$script("$(\"input:radio[name='vasc'][value='yes']\").parent().css('background-color', '#EDB6B2');"),
-      tags$script("$(\"input:radio[name='vasc'][value='no']\").parent().css('background-color', '#B2EDB5');")
+      tags$script(jsButtonColor("hyperT", "#EDB6B2", "yes")),
+      tags$script(jsButtonColor("hyperT", "#B2EDB5", "no")),
+      
+      tags$script(jsButtonColor("vasc", "#EDB6B2", "yes")),
+      tags$script(jsButtonColor("vasc", "#B2EDB5", "no"))
     ),
     
     # Show a plot of the generated distribution
@@ -112,10 +162,11 @@ ui <- fluidPage(
       tags$h3("Table for error checking"),
       br(),
       DT::dataTableOutput("table")
-      
     )
   )
 )
+# SERVER ------------------------------------------------------------------
+
 
 # Define server logic
 server <- function(input, output) {
@@ -138,43 +189,45 @@ server <- function(input, output) {
     
     # Order of subset arguments must be same order as new.col.order variable set in
     # intro.
-     # browser()
+    # browser()
     if (is.valid.age()) {
+      # browser()
       
-      dat.sub <- stroke.dt[.(input$user_age,
-                            input$sex,
-                            input$stroke,
-                            input$hf,
-                            input$diabetes,
-                            input$hyperT,
-                            input$vasc),
-                          range(stroke1y)]
-      cond <- dat.sub[2]-dat.sub[1]<0.001
-      if (cond){
+      dat.sub <- stroke.dt[age == input$user_age &
+                             female %in% evPar(input$sex) &
+                             stroke %in% evPar(input$stroke) &
+                             heartfailure%in% evPar(input$hf) &
+                             diabetes %in% evPar(input$diabetes) &
+                             hypertension %in% evPar(input$hyperT) &
+                             vascular %in% evPar(input$vasc),
+                           range(stroke1y)]
+      
+      one.value <- dat.sub[2]-dat.sub[1]<0.001
+      if (one.value){
         paste0(dat.sub[1], "%")
-      } else if (!cond) {
+      } else if (!one.value) {
         paste0("between ", dat.sub[1],
                "%", " and ",
                dat.sub[2],
                "%")
-  }
-
-           
+      }
+      
+      
     }
     else{
       paste0("Please enter your age (between 20 and 99)")
-      }
+    }
   })
-
+  
   output$table <- DT::renderDataTable(
-    DT::datatable(stroke.dt[.(input$user_age,
-                              input$sex,
-                              input$stroke,
-                              input$hf,
-                              input$diabetes,
-                              input$hyperT,
-                              input$vasc),
-                            1:10])
+    DT::datatable(stroke.dt[age == input$user_age &
+                              female %in% evPar(input$sex) &
+                              stroke %in% evPar(input$stroke) &
+                              heartfailure %in% evPar(input$hf) &
+                              diabetes %in% evPar(input$diabetes) &
+                              hypertension %in% evPar(input$hyperT) &
+                              vascular %in% evPar(input$vasc),
+                            1:3])
   )
 }
 
