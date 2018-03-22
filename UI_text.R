6d17f505ba3d043cf1f6af7057cad653ec322b97
Matthew Phelps <mPhelps@hjerteforeningen.dk>
2018-03-22 09:09:40 +0100

    Add UI text file

--- UI_text.R
+++ UI_text.R
@@ -10,6 +10,7 @@ in_age <- "Patient's age"
 in_sex <- "Patient's sex"
 in_stroke <- "Has patient ever had stroke"
 in_hf <- "Has patient ever had heart failire"
+in_hyperT <- "Has patient ever had hypertension"
 in_diab <- "Has patient ever had diabetes"
 in_vasc <- "Has patient ever had vascular disease"
 
@@ -19,4 +20,6 @@ in_vasc <- "Has patient ever had vascular disease"
 out_stroke <- "1-year stroke risk is estimated to be:"
 out_thomb <- "1-year thomboembolism risk is estimated to be:"
 
-
+# Text that appears when user has not entered an appropriate age (i.e. <20 ||
+# >99 || not a whole number)
+enter_age <- "Please enter a whole number between 20 - 99"
